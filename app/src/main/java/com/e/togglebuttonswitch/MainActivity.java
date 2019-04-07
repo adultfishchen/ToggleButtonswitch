63594f173a89705f9e1eb7ccced42a6b90d50e33
adultfishchen <adultfish17640336@gmail.com>
2019-04-07 20:06:11 +0800

    Initial commit

---
+++ app/src/main/java/com/e/togglebuttonswitch/MainActivity.java
@@ -0,0 +1,33 @@
+package com.e.togglebuttonswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ToggleButton tb=(ToggleButton)findViewById(R.id.toggleButton);
+        final Switch sw=(Switch)findViewById(R.id.Switch);
+
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this,"Toggle Button: "+tb.isChecked(),Toast.LENGTH_SHORT).show();
+            }
+                    });
+        sw.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this,"Switch:  "+sw.isChecked(),Toast.LENGTH_SHORT).show();
+            }
+            });
+    }
+}
